a0b815d47d13fe42af50157b0daab23b967f2e9c
Rogerio Alexandre <rogerioaba@gmail.com>
2012-09-24 22:47:11 -0300

    apagando init.js

--- app/assets/javascripts/ckeditor/init.js
+++
@@ -1,5 +0,0 @@
-(function() {
-  if (typeof window['CKEDITOR_BASEPATH'] === "undefined" || window['CKEDITOR_BASEPATH'] === null) {
-    window['CKEDITOR_BASEPATH'] = "/assets/ckeditor/";
-  }
-}).call(this);
