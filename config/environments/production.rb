fb7b3fd9cd9195dd4a27dd31a7b466747a192220
Rogerio Alexandre <rogerioaba@gmail.com>
2012-09-24 21:59:26 -0300

    tentando fazer o ckeditor funcionar

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Fanapolis::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.precompile += Ckeditor.assets
+  config.assets.precompile += %w( ckeditor/* )
   config.assets.compile = false
 
   # Generate digests for assets URLs
